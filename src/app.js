7abe390f15ebab021ff03b643dc47754e4a6e950
OgiNoegroho <oginugroho512@gmail.com>
2024-06-02 03:18:10 +0700

    more changes

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+app.listen( () => {
+  console.log(`Server is running on port`);
 });
